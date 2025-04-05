d9431c62fae5de059937e68945e2c2bc2ea08dc1
AotPiya <74890247+engkasuJ@users.noreply.github.com>
2025-04-05 12:22:33 +0700

    fix db

--- dags/example_dag.py
+++ dags/example_dag.py
@@ -1,9 +1,8 @@
 from airflow import DAG
-from airflow.operators.dummy import DummyOperator
+from airflow.operators.empty import EmptyOperator
 from datetime import datetime
 
-with DAG('example_dag', start_date=datetime(2025, 1, 1), schedule_interval=None) as dag:
-    start = DummyOperator(task_id='start')
-    end = DummyOperator(task_id='end')
+with DAG('example_dag', start_date=datetime(2025, 1, 1), schedule=None) as dag:
+    start = EmptyOperator(task_id='start')
+    end = EmptyOperator(task_id='end')
     start >> end
-    
